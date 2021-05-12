40e84fdc9b0d7f1a2579fc95a4a132f930c6615e
AvikaTrivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2021-05-13 01:42:53 +0530

    Update tgcalls.py

--- tgcalls/tgcalls.py
+++ tgcalls/tgcalls.py
@@ -1,7 +1,7 @@
 from pyrogram import Client
 from pytgcalls.pytgcalls import PyTgCalls
 
-import sira
+import queue
 import config
 
 
@@ -13,11 +13,11 @@ pytgcalls = PyTgCalls(client, 1512, False)
 def on_stream_end(chat_id: int) -> None:
     sira.task_done(chat_id)
 
-    if sira.is_empty(chat_id):
+    if queue.is_empty(chat_id):
         pytgcalls.leave_group_call(chat_id)
     else:
         pytgcalls.change_stream(
-            chat_id, sira.get(chat_id)["file_path"]
+            chat_id, queue.get(chat_id)["file_path"]
         )
 
 

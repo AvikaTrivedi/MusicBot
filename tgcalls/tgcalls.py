186fa80c7637331897f9a808d1e1e79f4bfac7e1
AvikaTrivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2021-05-13 09:37:59 +0530

    Update tgcalls.py

--- tgcalls/tgcalls.py
+++ tgcalls/tgcalls.py
@@ -11,7 +11,7 @@ pytgcalls = PyTgCalls(client, 1512, False)
 
 @pytgcalls.on_stream_end()
 def on_stream_end(chat_id: int) -> None:
-    sira.task_done(chat_id)
+    queue.task_done(chat_id)
 
     if queue.is_empty(chat_id):
         pytgcalls.leave_group_call(chat_id)
